ed819a5d0e0715b5ebbe2a86da91d180ba59c9fa
xaedes <xaedes@gmail.com>
2021-11-04 17:59:39 +0100

    Update inverse_orthogonal.h

--- inverse_orthogonal.h
+++ inverse_orthogonal.h
@@ -2,31 +2,119 @@
 
 #include <glm/glm.hpp>
 
-glm::mat4 inverse_orthogonal(const glm::mat4& m)
-{
-    // Based on compute_inverse<4, 4, T, Q, Aligned> from extern/glm/glm/detail/func_matrix.inl
-    // a lot of values in orthogonal matrices (see extern/glm/glm/ext/matrix_clip_space.inl) are zero,
-    // simplify compute_inverse<4, 4, T, Q, Aligned> using all known zeros of GLM_FUNC_QUALIFIER mat<4, 4, T, defaultp> ortho(T left, T right, T bottom, T top)
+namespace inverse_ortho {
+    // A lot of values in orthogonal matrices (see glm/glm/ext/matrix_clip_space.inl) are known.
+    // Simplify compute_inverse<4, 4, T, Q, Aligned> (see glm/glm/detail/func_matrix.inl) using all known values of the respective orthogonal matrix.
 
-    auto Coef00 = m[2][2];
-    auto Coef06 = m[1][1];
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoLRBT(::glm::mat<4, 4, T, Q> const& m)
+    {
+        // glm/glm/ext/matrix_clip_space.inl
+        // GLM_FUNC_QUALIFIER ::glm::mat<4, 4, T, defaultp> ortho(T left, T right, T bottom, T top)
+        // ::glm::mat<4, 4, T, defaultp> Result(static_cast<T>(1));
+        // Result[0][0] = static_cast<T>(2) / (right - left);
+        // Result[1][1] = static_cast<T>(2) / (top - bottom);
+        // Result[2][2] = - static_cast<T>(1);
+        // Result[3][0] = - (right + left) / (right - left);
+        // Result[3][1] = - (top + bottom) / (top - bottom);
+        ::glm::mat<4, 4, T, Q> Inverse(
+            -m[1][1], 0, 0, 0,
+            0, -m[0][0], 0, 0,
+            0, 0, m[0][0]*m[1][1], 0,
+            m[1][1]*m[3][0], m[0][0]*m[3][1], 0, -m[0][0]*m[1][1]
+        );
+        
+        T Determinant = (m[0][0] * Inverse[0][0]);
+        T OneOverDeterminant = static_cast<T>(1) / Determinant;
 
-    auto Coef08 = -m[3][1] * m[2][2];
-    auto Coef11 = m[1][1] * m[2][2];
+        return Inverse * OneOverDeterminant;
+    }
 
-    auto Coef16 = -m[3][0] * m[2][2];
-    auto Coef22 = -m[3][0] * m[1][1];
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoLRBTNF(::glm::mat<4, 4, T, Q> const& m)
+    {
+        // glm/glm/ext/matrix_clip_space.inl
+        // GLM_FUNC_QUALIFIER ::glm::mat<4, 4, T, defaultp> orthoLH_ZO(T left, T right, T bottom, T top, T zNear, T zFar)
+        // ::glm::mat<4, 4, T, defaultp> Result(1);
+        // Result[0][0] = static_cast<T>(2) / (right - left);
+        // Result[1][1] = static_cast<T>(2) / (top - bottom);
+        // Result[2][2] = static_cast<T>(1) / (zFar - zNear);
+        // Result[3][0] = - (right + left) / (right - left);
+        // Result[3][1] = - (top + bottom) / (top - bottom);
+        // Result[3][2] = - zNear / (zFar - zNear);
+        T Coef08 = -m[3][1] * m[2][2];
+        T Coef10 = m[1][1] * m[3][2];
+        T Coef11 = m[1][1] * m[2][2];
+        T Coef16 = -m[3][0] * m[2][2];
 
-    mat<4, 4, T, Q> Inverse(
-        m[1][1]*Coef00, 0, 0, 0,
-        0, m[0][0]*Coef00, 0, 0,
-        0, 0, m[0][0]*Coef06, 0,
-        m[1][1]*Coef16, m[0][0]*Coef08, 0, m[0][0]*Coef11
-    );
-    
-    auto Determinant = (m[0][0] * Inverse[0][0]);
-    T OneOverDeterminant = static_cast<T>(1) / Determinant;
+        ::glm::mat<4, 4, T, Q> Inverse(
+            m[1][1]*m[2][2], 0, 0, 0,
+            0, m[0][0]*m[2][2], 0, 0,
+            0, 0, m[0][0]*m[1][1],0,
+            m[1][1]*Coef16, m[0][0]*Coef08, -m[0][0]*Coef10, m[0][0]*Coef11
+        );
 
-    return Inverse * OneOverDeterminant;
-}
+        T Determinant = (m[0][0] * Inverse[0][0]);
+        T OneOverDeterminant = static_cast<T>(1) / Determinant;
+
+        return Inverse * OneOverDeterminant;
+    }
+
+    // remaining functions have the same implementation as inverse_orthoLRBTNF:
+
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoLH_ZO(::glm::mat<4, 4, T, Q> const& m)
+    {
+        return inverse_orthoLRBTNF(m);
+    }
+
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoLH_NO(::glm::mat<4, 4, T, Q> const& m)
+    {
+        return inverse_orthoLRBTNF(m);
+    }
+
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoRH_ZO(::glm::mat<4, 4, T, Q> const& m)
+    {
+        return inverse_orthoLRBTNF(m);
+    }
+
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoRH_NO(::glm::mat<4, 4, T, Q> const& m)
+    {
+        return inverse_orthoLRBTNF(m);
+    }
+
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoZO(::glm::mat<4, 4, T, Q> const& m)
+    {
+        return inverse_orthoLRBTNF(m);
+    }
+
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoNO(::glm::mat<4, 4, T, Q> const& m)
+    {
+        return inverse_orthoLRBTNF(m);
+    }
+
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoLH(::glm::mat<4, 4, T, Q> const& m)
+    {
+        return inverse_orthoLRBTNF(m);
+    }
+
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_orthoRH(::glm::mat<4, 4, T, Q> const& m)
+    {
+        return inverse_orthoLRBTNF(m);
+    }
+
+    template<typename T, ::glm::qualifier Q>
+    GLM_FUNC_DECL ::glm::mat<4, 4, T, Q> inverse_ortho(::glm::mat<4, 4, T, Q> const& m)
+    {
+        return inverse_orthoLRBTNF(m);
+    }
+
+} // namespace inverse_ortho
 
